6643ed466cc93550c2634254c21d089180bcf313
ganaraj <ganarajpr@gmail.com>
2015-09-10 13:32:55 +0100

    added testing

--- testing.js
+++ testing.js
@@ -2,17 +2,58 @@ var boxtree = require("./index");
 var chalk = require('chalk');
 var loadJsonFile = require('load-json-file');
 
-loadJsonFile('test/1.json')
+var typeToComponent = [
+    'Text',
+    'Image',
+    'Button',
+    'Input',
+    'Dropdown',
+    'Datepicker',
+    'Tabs',
+    'Region'
+];
+
+function convertTree(node,destNode){
+
+    if(node.type === "root"){
+        destNode = {
+            type : "Region",
+            children : []
+        };
+        for (var i = 0; i < node.children.length; i++) {
+            destNode.children.push(
+                convertTree(node.children[i],destNode)
+            );
+        }
+        return destNode;
+    }
+    else{
+        var newNode = {
+            type : typeToComponent[node.data[4]],
+            children : []
+        };
+        for (var i = 0; i < node.children.length; i++) {
+            newNode.children.push(
+                convertTree(node.children[i],newNode)
+            );
+        }
+        return newNode;
+    }
+}
+
+loadJsonFile('test/1_bb.json')
     .then(function (json) {
         return json.boxes.map(function (box) {
             return [
                 box.startX,
                 box.startY,
                 box.endX,
-                box.endY
+                box.endY,
+                box.type
             ];
         })
     })
     .then(function(boxes){
-        console.log(boxtree(boxes));
+        var tree = boxtree(boxes);
+        console.log(convertTree(tree));
     });
